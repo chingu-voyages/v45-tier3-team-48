de548dbdc43ce96feac7fd41e3abbd313d49e880
RTavarez <tavarezrainiery@yahoo.com>
2023-08-11 21:19:27 -0400

    added 'create-react-app' app as a frontend skeleton + added comments in app.js and index.js

---
+++ frontend/caregiver-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+    //functions that call to the backend go here
+
+    //buttons could initiate a route path change on click
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    This is a base for the caregiver app.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
