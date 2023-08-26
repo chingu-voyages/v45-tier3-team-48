ee353fe8204d9019d1a2e90cf3b6d8292a308c36
rtavarez98 <63325601+rtavarez98@users.noreply.github.com>
2023-08-25 23:18:04 -0400

    temp removal of individualGroups to resolve pull conflict

--- server/routes/individualGroups.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const individualGroupController = require('../controllers/individualGroupController');
-//Routes for individual groups
-router.get('/', individualGroupController.getIndividualGroup);
-router.post('/create', individualGroupController.createGroup);
-
-module.exports = router;
