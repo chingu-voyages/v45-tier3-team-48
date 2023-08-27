b060e8bfa1a82086be0bf22989ed4f46b135c39f
Johnathon Bowers <johnathonbowers@gmail.com>
2023-08-26 20:45:25 -0400

    add dotenv import statement to server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
+// importing the 'dotenv' package so to enable environment variables
+require('dotenv').config();
+
 // moved server to a separate file to make testing easier
 // the previous server file was renamed to app.js
 
 const app = require('./app');
 
-
 // updated so port is assigned if variable is undefined
 // + coerces strings into numbers
 const PORT = +process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
-    console.log(`Listening to PORT: ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Listening to PORT: ${PORT}`);
 });
 //
