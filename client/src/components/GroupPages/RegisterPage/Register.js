d299ae2e6f5211e56c6d08dbf0d3767e75b5668c
rrghiker <chris.ryan.hart1@gmail.com>
2023-09-02 23:05:50 -0400

    Added login route to fix merge conflict

--- client/src/components/GroupPages/RegisterPage/Register.js
+++ client/src/components/GroupPages/RegisterPage/Register.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import UserContext from './/UserContext';
+// import UserContext from './/UserContext';
 import './register.css';
 
 function Register() {
