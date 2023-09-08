982e64fb1bd9a6fbb9dc250ad382753961e73b16
Johnathon Bowers <johnathonbowers@gmail.com>
2023-09-07 21:40:22 -0400

    add findAllRequestsForOneGroup function

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -7,4 +7,16 @@ module.exports = {
             .then(createdRequest => res.json(createdRequest))
             .catch(err => res.status(400).json(err));
     },
+    // find all requests associated with a group
+    findAllRequestsForOneGroup: (req, res) => {
+        Request.find({ groupId: req.params.groupId })
+            .sort({ dateTimeUTC: 'asc' })
+            .then(allRequests => res.json(allRequests))
+            .catch(err => res.status(400).json(err));
+    },
+    
+    // sign up for a request as a support person
+    signUpForRequest: (req, res) => {
+        
+    }
 };
