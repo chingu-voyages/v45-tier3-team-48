f75fba74159aabdfc8cd48265b33dc8e65d926b1
Johnathon Bowers <johnathonbowers@gmail.com>
2023-09-09 23:59:16 -0400

    add request display table component

---
+++ client/src/components/GroupPages/RequestPage/RequestDisplayTable.js
@@ -0,0 +1,83 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import CaregiverApi from '../../../api';
+import UserContext from '../../../UserContext';
+
+const RequestDisplayTable = () => {
+    
+    // grab groupId from parameter variable
+    const { groupId } = useParams();
+
+    const { userId } = useContext(UserContext);
+
+    const [userInfo, setUserInfo] = useState({})
+
+    const [requests, setRequests] = useState();
+
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        async function getRequests(groupId) {
+            let res = await CaregiverApi.findAllRequestsForOneGroup(groupId);
+            setRequests(res);
+            setIsLoaded(true);
+        }
+        // TODO Add function to load user info to get user's auth level for this group
+        getRequests(groupId);
+    }, [groupId]);
+
+    const formatDate = (isoString) => {
+        const dateObject = new Date(isoString);
+        return dateObject.toLocaleDateString({}, {day: '2-digit', month: '2-digit', year: '2-digit'})
+    }
+
+    const formatTime = (isoString) => {
+        const dateObject = new Date(isoString);
+        return dateObject.toLocaleTimeString({}, {hour: 'numeric', minute: '2-digit'});
+    }
+
+    // dummy data for testing
+    // const user = {
+    //     groupData: [
+    //         groupId: 
+    //     ]
+    // }
+
+    return (
+        <>
+            {isLoaded && (
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Date Needed</td>
+                            <td>Time Needed</td>
+                            <td>Category</td>
+                            <td>Description</td>
+                            <td>Assigned To</td>
+                            <td>Actions</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {requests.map((request, index) => {
+                            return(
+                                <tr key={index}>
+                                    <td>{formatDate(request.dateTimeUTC)}</td>
+                                    <td>{formatTime(request.dateTimeUTC)}</td>
+                                    <td>{request.category}</td>
+                                    <td>{request.description}</td>
+                                    {request.assignedTo ? 
+                                        <td>{request.assignedTo.fullName}</td> :
+                                        <td></td>
+                                    }
+                                    {}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            )}
+        </>
+    )
+}
+
+export default RequestDisplayTable
