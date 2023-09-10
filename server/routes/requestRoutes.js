67857f8e1d57e2f7e56f260c48a83d2535e35b05
Johnathon Bowers <johnathonbowers@gmail.com>
2023-09-09 23:58:35 -0400

    add route for displaying all group requests

--- server/routes/requestRoutes.js
+++ server/routes/requestRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const RequestController = require('../controllers/requestController');
 
 // routes for requests
-
+router.get('/:groupId/getall', RequestController.findAllRequestsForOneGroup);
 router.post('/create', RequestController.createNewRequest);
 
 module.exports = router;
