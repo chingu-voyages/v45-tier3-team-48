b0334858061a3dbf0698e79acc7c318b6169517c
Johnathon Bowers <johnathonbowers@gmail.com>
2023-09-13 19:20:07 -0400

    add back-end route to get user info

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 //Routes for main examples: landing page, login/register
 router.post('/register', homeController.createUser);
+router.get('/user/getInfo/:userId', homeController.getUserInfo);
 
 module.exports = router;
