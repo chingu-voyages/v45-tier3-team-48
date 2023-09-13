167ec4f13ab94079ee424078663cb5391c570457
rrghiker <chris.ryan.hart1@gmail.com>
2023-09-13 10:55:33 -0400

    Deleted unrelated example methods from frontend api.

--- client/src/api.js
+++ client/src/api.js
@@ -26,10 +26,6 @@ class CaregiverApi {
         }
     }
 
-    // Individual API routes
-
-    // Insert new routes here:
-
     static async loginUser(loginData) {
         let res = await this.request('login', loginData, 'post');
         console.log(loginData);
@@ -116,45 +112,7 @@ class CaregiverApi {
         }
     }
 
-    /**
-     *
-     * Example routes shown below from past project
-     */
-
-    // static async getMeetups() {
-    //   // consider adding more filtering later
-    //   let res = await this.request(`meetups`);
-    //   return res;
-    // }
-
-    // static async leaveMeetup(meetup_id){
-    //   let res = await this.request(`meetups/${meetup_id}/leave`,{},'delete')
-    //   return res;
-    // }
-
-    // static async login(loginData){
-    //   try{
-    //     let res = await this.request(`login`,loginData,'post');
-    //     return res;
-    //   }catch(err){
-    //     console.log('Heres my error:',err);
-    //     throw err;
-    //   }
-
-    // }
-
-    // static async createMeetup(meetupData,formattedDateTime){
-    //   try{
-    //     meetupData['date_time_utc']=formattedDateTime;
-    //     let res = await this.request('meetups/new',meetupData,'post');
-    //     return res;
-    //   }catch(err){
-    //     console.log('Heres my error:',err);
-    //     return err;
-    //   }
-    // }
 }
 
-// for now, put token ("testuser" / "password" on class)
 
 export default CaregiverApi;
