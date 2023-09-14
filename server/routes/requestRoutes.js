28042937a29f97f392fb94f19878e5d1833273dd
Johnathon Bowers <johnathonbowers@gmail.com>
2023-09-14 15:12:46 -0400

    add back-end routes for request find, edit, and delete

--- server/routes/requestRoutes.js
+++ server/routes/requestRoutes.js
@@ -4,6 +4,9 @@ const RequestController = require('../controllers/requestController');
 
 // routes for requests
 router.get('/:groupId/getall', RequestController.findAllRequestsForOneGroup);
+router.get('/:requestId', RequestController.findOneRequest);
 router.post('/create', RequestController.createNewRequest);
+router.put('/edit/:requestId', RequestController.updateOneRequest);
+router.delete('/delete/:requestId', RequestController.deleteOneRequest);
 
 module.exports = router;
