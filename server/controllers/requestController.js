bf4e86e9ba49153b82aa628c2dc2847baac52eb6
Johnathon Bowers <johnathonbowers@gmail.com>
2023-09-14 15:12:12 -0400

    add controller methods for request find, edit, and delete

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -14,9 +14,29 @@ module.exports = {
             .then(allRequests => res.json(allRequests))
             .catch(err => res.status(400).json(err));
     },
-    
+    // find one request
+    findOneRequest: (req, res) => {
+        Request.findById(req.params.requestId)
+        .then(oneRequest => res.json(oneRequest))
+        .catch(err => res.status(400).json(err));
+    },
     // sign up for a request as a support person
     signUpForRequest: (req, res) => {
         
+    },
+    // allow caregiver to edit a request
+    updateOneRequest: (req, res) => {
+        Request.findByIdAndUpdate(req.params.requestId, req.body, {
+            new: true,
+            runValidators: true
+        })
+            .then(updatedRequest => res.json(updatedRequest))
+            .catch(err => res.status(400).json(err));
+    },
+    // allow a caregiver to delete a request
+    deleteOneRequest: (req, res) => {
+        Request.findByIdAndDelete(req.params.requestId)
+            .then(deleteConfirmation => res.json(deleteConfirmation))
+            .catch(err => res.status(400).json(err));
     }
 };
