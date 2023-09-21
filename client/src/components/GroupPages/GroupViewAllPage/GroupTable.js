39595733c92fd2e9f5a0e1cb1293b990f72c1b24
RTavarez <tavarezrainiery@yahoo.com>
2023-09-21 14:17:14 -0400

    correctly aligned and spaced components in groupViewAll, groupEditDelete, and groupCreation pages

--- client/src/components/GroupPages/GroupViewAllPage/GroupTable.js
+++ client/src/components/GroupPages/GroupViewAllPage/GroupTable.js
@@ -16,7 +16,7 @@ const GroupTable = ( ) => {
         fetchData();
     }, []);
 
-    return (
+    return (//mx-[300px]
         <div className="font-general bg-gray-50 h-max w-full pt-[50px]">
             <div className="flex justify-between items-center mx-[150px] pb-[50px] border-b-[4px] border-gray-300">
                 <h2 className="text-[30px]">All Groups</h2>
@@ -24,14 +24,14 @@ const GroupTable = ( ) => {
                     Create new group
                 </button>
             </div>
-            <table className="mx-[300px] my-[50px]">
+            <table className="flex flex-col items-center mx-80 py-[50px]">
                 <tbody>
                     {groups.map((group, index) => (
                         <tr className="border-b-[2px] border-gray-300" key={index}>
                             <td className="mb-[25px]">
                                 <p className="rounded-[12px] py-[12px] px-[35px] bg-light-mint text-[75px] text-dark-green my-[25px] mr-[25px]">{Array.from(group.namePatient)[0]}</p>
                             </td>
-                            <td>
+                            <td className="w-screen">
                                 <div className="flex justify-between">
                                     <h2 className="text-[20px]">{group.namePatient}</h2>
                                     <button className="rounded-[12px] py-[5px] px-[35px] bg-dark-green text-white" onClick={ () => navigate("/GroupViewSingle/" + group._id ) }>See More</button>
