bbf8facec65faa4ffad0d5b001894174c7b8aaf5
rrghiker <chris.ryan.hart1@gmail.com>
2023-09-21 10:32:29 -0400

    Corrected variable passed into ChildGroupTable to be 'groupInfo'

--- client/src/components/GroupPages/GroupViewAllPage/UserGroups.js
+++ client/src/components/GroupPages/GroupViewAllPage/UserGroups.js
@@ -6,32 +6,12 @@ import axios from 'axios';
 import validator from 'validator';
 import ChildGroupTable from './ChildGroupTable';
 
-// make conditional to show only user groups or all groups
-
-// have to handle no groups on the usersGroups page
-// if '/userGroups' and groupCount = false, then show 
-
-
 
 const UserGroups = () => {
 
-    // extract groupInfo from App
     const { groupInfo } = useContext(UserContext);
     
     const navigate = useNavigate();
-
-    // have to make 2 database calls because user data isn't included with the groups
-    // could get user groups from useContext as another choice
-    // usecontext has a tradeoff, because state needs to be updated each time across multiple components
-    // const fetchUserGroupData = async () => {
-        // can retrieve from useContext var
-        // update groups through setGroups
-        
-        // groups = [groupInfo];
-        // useLocation to know which update to do
-
-    //     console.log('test');
-    // }
 
     return (
         <div>
@@ -41,7 +21,7 @@ const UserGroups = () => {
                     Create new group
                 </button>
             </div>
-            <ChildGroupTable groups={groups}/>
+            <ChildGroupTable groups={groupInfo}/>
         </div>
     );
 }
