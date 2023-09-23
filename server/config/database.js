e69c2cd54631b023cb7ddc1e15fb387fb5ee1b23
Johnathon Bowers <109883576+JohnathonBowers@users.noreply.github.com>
2023-09-22 23:01:40 -0400

    Change database string back to env variable

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect('mongodb+srv://ChinguProject:Team48@cluster0.uinzldr.mongodb.net/?retryWrites=true&w=majority',{
+        const conn = await mongoose.connect(process.env.DB_STRING,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
