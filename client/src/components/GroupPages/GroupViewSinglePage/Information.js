2365379c12f9918da7a6288d9d7bf4a88b3e0583
rrghiker <chris.ryan.hart1@gmail.com>
2023-10-04 12:21:58 -0400

    Added Mobile formatting for individual group description

--- client/src/components/GroupPages/GroupViewSinglePage/Information.js
+++ client/src/components/GroupPages/GroupViewSinglePage/Information.js
@@ -81,6 +81,33 @@ const Information = () => {
         <>
             {isLoaded && (
                 <>
+                    {/* <div style={{width: 1101, height: 228, left: 169, top: 154, position: 'absolute'}}>
+                        <div style={{width: 1101, height: 0, left: 0, top: 228, position: 'absolute', border: '4px #D1D5DB solid'}}></div>
+                        <div style={{left: 214, top: 0, position: 'absolute', color: 'black', fontSize: 36, fontFamily: 'Open Sans', fontWeight: '600', lineHeight: 40, wordWrap: 'break-word'}}>Jane Smith</div>
+                        <div style={{left: 214, top: 73, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Open Sans', fontStyle: 'italic', fontWeight: '600', lineHeight: 28, wordWrap: 'break-word'}}>Caregiver:</div>
+                        <div style={{left: 214, top: 117, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Open Sans', fontStyle: 'italic', fontWeight: '600', lineHeight: 28, wordWrap: 'break-word'}}>Description:</div>
+                        <div style={{width: 764, height: 41, left: 337, top: 125, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Open Sans', fontWeight: '400', lineHeight: 28, wordWrap: 'break-word'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et dictum nibh. Cas laoreet arcu. Nulla sed commodo velit, luctus velit. Pellentesque elemenm.</div>
+                        <div style={{left: 337, top: 73, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Open Sans', fontWeight: '400', lineHeight: 28, wordWrap: 'break-word'}}>John Doe</div>
+                        <div style={{width: 205, height: 58, left: 896, top: 0, position: 'absolute'}}>
+                            <div style={{width: 205, height: 58, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 125, border: '2px #638262 solid'}} />
+                            <div style={{width: 123.58, height: 26, left: 40.71, top: 16, position: 'absolute', textAlign: 'center', color: '#638262', fontSize: 20, fontFamily: 'Open Sans', fontWeight: '400', lineHeight: 28, wordWrap: 'break-word'}}>Edit Details</div>
+                        </div>
+                    </div> */}
+{/* border-t-2 border-gray-300 border-b-2 lg:border-b-0 py-10 */}
+                    <div className="border-t-2 border-gray-300 border-b-2 lg:border-b-0 py-10">
+                        <div className="flex justify-center ">
+                            <img className="w-[140px] xs:w-[170px] sm:w-[200px]" src="https://images.unsplash.com/photo-1548536646-b8bebe870011?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80"/>
+                        </div>
+                        <div style={{color: 'black', fontSize: 36, fontFamily: 'Open Sans', fontWeight: '600', wordWrap: 'break-word'}}>{groupData.namePatient}</div>
+                        {/* cgiver/des */}
+                        <div className=''>
+                            <div className="font-normal px-5 text-lg [font-family:'Open_Sans'] text-left"><span className='font-semibold italic'>Caregiver:  </span>{groupData.nameCaregiver}</div>
+                            <div className="font-normal px-5 text-lg [font-family:'Open_Sans'] text-left"><span className='font-semibold italic'>Description:  </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et dictum nibh. Cas laoreet arcu. Nulla sed commodo velit, luctus velit. Pellentesque elemenm.</div>
+                        </div>
+                        <div className='pt-2'>
+                            <button className="border-[2px] rounded-[125px] py-[8px] px-[15px] md:py-[15px] md:px-[35px] border-dark-green bg-gray-50 text-dark-green hover:bg-dark-green hover:text-white transition-color duration-300">Edit Group Details</button>
+                        </div>
+                    </div>
                     <div>
                         <h1>{groupData.namePatient}</h1>
                         {(!userRole || userRole === "Caregiver") ?
@@ -100,6 +127,7 @@ const Information = () => {
                             <p>Click the "Add Request" button to begin adding support requests to your group.</p> :
                             <p>No support requests yet.</p>
                     }
+
                     
                 </>
             )}
