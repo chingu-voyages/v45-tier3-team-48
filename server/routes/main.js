5eda8827a5f77c540dbced1fdcace7cc2f9aea3c
rrghiker <chris.ryan.hart1@gmail.com>
2023-10-05 13:20:23 -0400

    Added middleware to confirm user is logged in

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
+const { ensureLoggedIn } = require('../middleware/authorization');
+
 //Routes for main examples: landing page, login/register
-router.post('/register', homeController.createUser);
-router.get('/user/getInfo/:userId', homeController.getUserInfo);
-router.get('/editUser/:id', homeController.getUserProfile);
-router.patch('/editUser/:id', homeController.updateUserProfile);
+router.post('/register', ensureLoggedIn, homeController.createUser);
+router.get('/user/getInfo/:userId', ensureLoggedIn, homeController.getUserInfo);
+router.get('/editUser/:id', ensureLoggedIn, homeController.getUserProfile);
+router.patch('/editUser/:id', ensureLoggedIn, homeController.updateUserProfile);
 
 module.exports = router;
