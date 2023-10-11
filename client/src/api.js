d8bfa54d0f0b0ac3dd73a2db1828c0edde7299e0
Johnathon Bowers <johnathonbowers@gmail.com>
2023-10-11 16:09:33 -0400

    remove console log
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- client/src/api.js
+++ client/src/api.js
@@ -59,7 +59,6 @@ class CaregiverApi {
 
     static async registerUser(userData) {
         let res = await this.request('register',userData,'post');
-        console.log('in Cgiver api register');
         if (res.token) {
             // Store the token in the class
             this.token = res.token;
