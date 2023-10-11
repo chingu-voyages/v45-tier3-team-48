b0d5bae045ee98993fa3382f3b7adffb4f335282
Johnathon Bowers <johnathonbowers@gmail.com>
2023-10-11 12:44:04 -0400

    remove auth middleware from register route

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,7 @@ const homeController = require('../controllers/homeController');
 const { ensureLoggedIn } = require('../middleware/authorization');
 
 //Routes for main examples: landing page, login/register
-router.post('/register', ensureLoggedIn, homeController.createUser);
+router.post('/register', homeController.createUser);
 router.get('/user/getInfo/:userId', ensureLoggedIn, homeController.getUserInfo);
 router.get('/editUser/:id', ensureLoggedIn, homeController.getUserProfile);
 router.patch('/editUser/:id', ensureLoggedIn, homeController.updateUserProfile);
